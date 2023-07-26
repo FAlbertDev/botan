451f135daf23e5ef75eefe0d9cbb2c076605f63d
Fabian Albert <fabian.albert@rohde-schwarz.com>
2023-07-26 15:38:35 +0200

    ci adaptions

--- src/scripts/ci/ci_tlsanvil_test.py
+++ src/scripts/ci/ci_tlsanvil_test.py
@@ -10,16 +10,6 @@ import os
 import subprocess
 
 
-"""
-java -jar ./tlsanvil/TLS-Testsuite/apps/TLS-Testsuite.jar \
-   -strength 1 \
-   -parallelHandshakes 1 \
-   -testPackage both.tls12.rfc5246.Fragmentation.sendZeroLengthApplicationRecord \
-   -ignoreCache \
-   server \
-   -connect 127.0.0.1:4433
-"""
-
 class Config:
     """ Hardcoded configurations for this CI script """
     key_and_cert_storage_path = "/tmp/"
@@ -28,6 +18,7 @@ class Config:
     tmp_cert_file_name = "tmp_rsa_cert.pem"
     server_dest_ip = "127.0.0.1"
     server_dest_port = 4433
+    botan_server_log = "./logs/botan_server.log"
 
 
 def create_cert_and_key(botan_exe_path):
@@ -57,8 +48,9 @@ def server_test(botan_exe_path: str, tls_anvil_jar_path: str):
         "java", "-jar", tls_anvil_jar_path,
         "-strength", "1",
         "-parallelHandshakes", "1",
+        "-disableTcpDump",
         "-outputFolder", Config.test_suite_results_path,
-        "-testPackage", "both.tls12.rfc5246.Fragmentation.sendZeroLengthApplicationRecord", # tmp
+        "-testPackage", "de.rub.nds.tlstest.suite.tests.server.tls13.rfc8446.ClientHello", # tmp
         "server", "-connect", f"{Config.server_dest_ip}:{Config.server_dest_port}"
     ]
 
@@ -66,11 +58,13 @@ def server_test(botan_exe_path: str, tls_anvil_jar_path: str):
         botan_exe_path, "tls_server", cert_path, key_path, f"--port={Config.server_dest_port}"
     ]
 
-    # Run Botan and test is with TLS-Anvil
-    botan_server_process = subprocess.Popen(botan_server_cmd)
-    subprocess.run(tls_anvil_cmd, check=True)
+    os.makedirs(os.path.dirname(Config.botan_server_log), exist_ok=True)
 
-    botan_server_process.kill()
+    # Run Botan and test is with TLS-Anvil
+    with open(Config.botan_server_log, 'w', encoding='utf-8') as server_log_file:
+        botan_server_process = subprocess.Popen(botan_server_cmd, stdout=server_log_file, stderr=server_log_file)
+        subprocess.run(tls_anvil_cmd, check=True)
+        botan_server_process.kill()
 
 
 def client_test(botan_exe_path: str, tls_anvil_jar_path: str):
