8dddf811c0b2557d907577b29ccade9a73c866c4
Fabian Albert <fabian.albert@rohde-schwarz.com>
2023-07-28 08:37:50 +0200

    print correct

--- src/scripts/ci/ci_tlsanvil_check.py
+++ src/scripts/ci/ci_tlsanvil_check.py
@@ -118,7 +118,8 @@ def process_results_container(results_container_path: str):
                 logging.debug("%s: '%s' -> ok", method_id, result)
                 success = True
             else:
-                print("::group::%s: Unexpected result '%s'.\n\n%s\n::endgroup::", method_id, result, failing_test_info(json_data))
+                #logging.info("%s: Unexpected result '%s'.\n\n%s", method_id, result, failing_test_info(json_data))
+                print(f"{method_id}: Unexpected result '{result}'{failing_test_info(json_data)}")
 
         except KeyError:
             logging.error("Json file '%s' has missing entries.", results_container_path)
